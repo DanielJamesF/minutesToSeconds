71a8de6502487e2427ac26547b61f4955a32ca47
DanielJamesF <fredericksd17@gmail.com>
2022-06-17 10:13:21 +0200

    first commit

---
+++ script/code.js
@@ -0,0 +1,6 @@
+const btnDisplay = document.getElementById('result');
+btnDisplay.addEventListener('click',()=>{
+    let minutes = document.getElementById('min').value;
+    let seconds = minutes*60;
+    document.getElementById('sec').innerHTML = `${seconds} seconds`;
+})
